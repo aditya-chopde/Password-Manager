67a4d4f5d20558e8bb30aeec8ae6218d795909e4
Aditya Chopde <its.adityac@gmail.com>
2025-01-23 21:48:16 +0530

    made some fixes in the navbar

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -27,18 +27,18 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="flex justify-between mx-8 items-center">
-        <div onClick={loggedOut}>
+      <nav className="flex justify-between mx-8 items-center lg:justify-around">
+        <div onClick={loggedOut} className="cursor-pointer">
           <img
             src={svgs.logout}
             alt="logout-icon"
             className={`w-8 invert ${isLogin ? "" : "hidden"}`}
           />
         </div>
-        <div>
+        <div className="cursor-pointer">
           <img src={images.logo} alt="passpod_logo" className="w-32" />
         </div>
-        <div onClick={() => navigate("/dashboard")}>
+        <div onClick={() => navigate("/dashboard")} className="cursor-pointer">
           <img
             src={svgs.user}
             alt="user-icon"
